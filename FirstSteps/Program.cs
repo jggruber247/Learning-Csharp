f3aed3df97d3cc29b31d7237c13837a9b33a1ac4
jggruber247 <jggruber247@gmail.com>
2021-11-19 13:19:23 -0500

    Okay I think I remember now

--- FirstSteps/Program.cs
+++ FirstSteps/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string strOne = "World";
+string strOne = "World!";
+Console.WriteLine($"Hello, {strOne}");
+strOne = "everyone!";
 Console.WriteLine($"Hello, {strOne}");
