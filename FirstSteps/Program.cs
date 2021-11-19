ac5c45ee19f40a64f0b43ee0a7b6584008f1e789
jggruber247 <jggruber247@gmail.com>
2021-11-19 13:33:03 -0500

    Commit 5

--- FirstSteps/Program.cs
+++ FirstSteps/Program.cs
@@ -6,3 +6,8 @@ strOne = "everyone!";
 Console.WriteLine($"Hello, {strOne}");
 
 // Working with Strings
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
