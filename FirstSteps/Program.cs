8ed95650e338f87620a7f088fdeb377631cf6ad2
jggruber247 <jggruber247@gmail.com>
2021-11-24 10:43:12 -0500

    Commit 6

--- FirstSteps/Program.cs
+++ FirstSteps/Program.cs
@@ -1,13 +1,25 @@
-﻿// Hello World
+﻿// Hello World and String interpolation
 Console.WriteLine("Hello, World!");
 string strOne = "World!";
 Console.WriteLine($"Hello, {strOne}");
 strOne = "everyone!";
 Console.WriteLine($"Hello, {strOne}");
 
-// Working with Strings
+// Working with Strings: Length method
 string firstFriend = "Maria";
 string secondFriend = "Sage";
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
 Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
 Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+// Do more with strings: Trim methods
+string greeting = "      Hello World!      ";
+Console.WriteLine($"[{greeting}]");
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+// Do more with strings: Replace method
