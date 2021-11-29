237fc72e5c888e3ea8fd48f34239d7864c6afc20
jggruber247 <jggruber247@gmail.com>
2021-11-29 13:27:09 -0500

    Commit 15

--- FirstSteps/NumbersPractice.cs
+++ FirstSteps/NumbersPractice.cs
@@ -4,6 +4,16 @@ public class NumbersPractice
 {
 	public NumbersPractice()
 	{
-
+		// Explore integer math
+		int a = 18;
+		int b = 6;
+		int c = a + b;
+		Console.WriteLine("18 + 6 = " + c);
+		c = a - b;
+		Console.WriteLine("18 - 6 = " + c);
+		c = a * b;
+		Console.WriteLine("18 * 6 = " + c);
+		c = a / b;
+		Console.WriteLine("18 / 6 = " + c);
 	}
 }
