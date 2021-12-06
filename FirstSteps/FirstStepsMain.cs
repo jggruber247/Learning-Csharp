b6568f3c2a8715e0e3f24d294c32399cf9f778b0
jggruber247 <jggruber247@gmail.com>
2021-12-06 11:34:28 -0500

    Commit 22

--- FirstSteps/FirstStepsMain.cs
+++ FirstSteps/FirstStepsMain.cs
@@ -88,6 +88,11 @@ public class FirstSteps
         int e = (a + b) % c;
         Console.WriteLine($"quotient: {d}");
         Console.WriteLine($"remainder: {e}");
+        int max = int.MaxValue;
+        int min = int.MinValue;
+        Console.WriteLine($"The range of integers is {min} to {max}");
+        int what = max + 3;
+        Console.WriteLine($"An example of overflow: {what}");
     }
 
 	public static void Main(string[] args)
